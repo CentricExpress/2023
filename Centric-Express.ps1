3a81e5d58ef76981c9c3e781ec364da59fbac8f5
CentricExpress <130205342+CentricExpress@users.noreply.github.com>
2023-04-08 08:47:24 +0300

    Add files via upload

---
+++ Centric-Express.ps1
@@ -0,0 +1,11 @@
+﻿Set-ExecutionPolicy Bypass -Scope Process -Force
+$IsChocoInstalled = Get-Command -Name choco.exe -ErrorAction SilentlyContinue
+
+If (!$IsChocoInstalled){
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+choco install prometheus-windows-exporter.install -y
+
+#choco uninstall git.install -y
+#choco uninstall prometheus-windows-exporter.install -y
